2ccc34e64a8c4760ee10749b017ff37f916796b8
Chris Noden <chris@noden.net>
2022-11-04 15:24:40 +0000

    Update scroll position bug test

--- app.vue
+++ app.vue
@@ -8,7 +8,7 @@
 
         <footer style="padding-top: 100px; display: flex; flex-direction: column;">
             <NuxtLink style="padding: 10px 0; font-size: 18px;" to="/">Home</NuxtLink>
-            <NuxtLink style="padding: 10px 0; font-size: 18px;" to="/page2">Page 2</NuxtLink>
+            <NuxtLink style="padding: 10px 0; font-size: 18px;" to="/page2?foo=bar">Page 2</NuxtLink>
         </footer>
     </div>
 
